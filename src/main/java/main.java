0a9fe7cdd47452563ca426405a55546646442880
bobbyjoedoodlee <65097721+bobbyjoedoodlee@users.noreply.github.com>
2020-06-13 14:03:13 -0700

    Update main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -7,7 +7,7 @@ public class main {
 
     public static void main(String args[]) throws Exception {
 
-        JDA jda = new JDABuilder("NzIxNDUzMzE2NjIxNTk4ODIw.XuUvzA.eViyxHepntuS1Rkq3H159tziRCo").build();
+        JDA jda = new JDABuilder("NzIxNDUzMzE2NjIxNTk4ODIw.XuU-3Q.HrOQAKZsMkgGkSwjOIEPBg0gkTU").build();
 
         jda.addEventListener(new commands());
         jda.addEventListener(new embeds()
